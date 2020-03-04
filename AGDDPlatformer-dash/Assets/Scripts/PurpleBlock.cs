d2f7c436a760b5d0231f3ef21a89162ac63965e7
Bjarni <bjarnib18@ru.is>
2020-03-04 18:02:48 +0000

    Game Added

---
+++ AGDDPlatformer-dash/Assets/Scripts/PurpleBlock.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PurpleBlock : MonoBehaviour
+{
+    private AudioLowPassFilter sound;
+    private void Awake() 
+    {
+        sound = GameObject.Find("Main Camera").GetComponent<AudioLowPassFilter>();
+    }
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (other.gameObject.tag == "Player1") sound.cutoffFrequency = 2000f;
+    }
+    private void OnTriggerStay2D(Collider2D other) 
+    {
+        if (other.gameObject.tag == "Player1")
+        {
+            other.gameObject.GetComponent<AGDDPlatformer.PlayerController>().setInZone(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) 
+    {
+        if (other.gameObject.tag == "Player1")
+        {
+            sound.cutoffFrequency = 5007;
+            other.gameObject.GetComponent<AGDDPlatformer.PlayerController>().setInZone(false);
+        }    
+    }
+}
